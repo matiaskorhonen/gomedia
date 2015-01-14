cf8191d9c1e01b751117f3e23ce3f7c81cdebae4
Matias Korhonen <matias@kiskolabs.com>
2015-01-14 20:43:09 +0200

    Stream the received file to S3.

--- gomedia.go
+++ gomedia.go
@@ -4,14 +4,16 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"math/rand"
 	"net/http"
 	"regexp"
 	"strconv"
 	"time"
 
-	"github.com/mitchellh/goamz/aws"
-	"github.com/mitchellh/goamz/s3"
+	"github.com/crowdmob/goamz/aws"
+	"github.com/crowdmob/goamz/s3"
+	"github.com/goji/httpauth"
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
 )
@@ -31,62 +33,62 @@ func rootHandler(c web.C, w http.ResponseWriter, r *http.Request) {
 }
 
 func tweetbot(c web.C, w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(r.ContentLength)
-
-	// message := r.Form["message"]
-	// source := r.Form["source"]
-	file, header, err := r.FormFile("media")
-	defer file.Close()
-
+	multiReader, err := r.MultipartReader()
 	if err != nil {
-		panic(err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
-	timeStamp := time.Now().Unix()
-	random := rand.Intn(999999)
-	filename := fmt.Sprintf("%x-%x-%s", timeStamp, random, header.Filename)
-
+	// Open Bucket
 	auth, err := aws.EnvAuth()
 	if err != nil {
 		panic(err.Error())
 	}
-
-	// Open Bucket
 	s := s3.New(auth, aws.EUWest)
 	bucket := s.Bucket(bucketName)
 
+	part, err := multiReader.NextPart()
+	if err == io.EOF {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	originalFilename := part.FileName()
+	timeStamp := time.Now().Unix()
+	randomValue := rand.Intn(999999)
+	filename := fmt.Sprintf("%x-%x-%s", timeStamp, randomValue, originalFilename)
 	path := fmt.Sprintf("tweetbot/%s", filename)
 
-	contentLength, err := strconv.Atoi(header.Header.Get("Content-Length"))
+	contentType := part.Header.Get("Content-Type")
+	contentLength, err := strconv.ParseInt(part.Header.Get("Content-Length"), 10, 64)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = bucket.PutReader(path, part, contentLength, contentType, s3.PublicRead, s3.Options{CacheControl: "public, max-age=315360000"})
 	if err != nil {
 		panic(err.Error())
 	}
 
-	buffer := make([]byte, contentLength)
-	cBytes, err := file.Read(buffer)
-
-	s3Headers := map[string][]string{"Content-Type": {header.Header.Get("Content-Type")}, "Cache-Control": {"public, max-age=315360000"}}
-
-	err = bucket.PutHeader(path, buffer[0:cBytes], s3Headers, s3.PublicRead)
-	if err != nil {
-		panic(err.Error())
-	}
+	fmt.Printf("\nFile %s (%s) uploaded successfully.\n", originalFilename, path)
 
 	url := fmt.Sprintf("%s/%s", baseURL, path)
 
-	fmt.Printf("\nFile %s (%s) uploaded successfully.\n", header.Filename, header.Header)
-
 	responseMap := map[string]string{"url": url}
 	jsonResponse, _ := json.Marshal(responseMap)
-	fmt.Println(string(jsonResponse))
 
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprintf(w, string(jsonResponse))
 }
 
 func main() {
+	goji.Use(httpauth.SimpleBasicAuth("x", "password"))
+
 	goji.Get("/", rootHandler)
+
 	re := regexp.MustCompile("/tweetbot")
 	goji.Post(re, tweetbot)
+
 	goji.Serve()
 }
